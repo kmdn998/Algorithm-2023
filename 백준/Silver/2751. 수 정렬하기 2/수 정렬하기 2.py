dd41c49f6df48bb1e8df68da864c7f93a57065b9
kim do yeon <doyeon7507@gmail.com>
2023-10-11 14:08:56 +0900

    Update 수 정렬하기 2.py

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,9 +1,38 @@
-N = int(input())
-arr = []
+n=int(input())
+li=[]
 
-for i in range(N):
-  arr.append(int(input()))
+for i in range(n):
+    li.append(int(input()))
 
-arr.sort()
-for i in arr:
-  print(i)
+def sort(arr):
+    if len(arr)<2:
+        return arr
+    
+    mid=len(arr)//2
+    left=sort(arr[:mid])
+    right=sort(arr[mid:])
+
+    return merge(left,right)
+    
+def merge(left,right):
+    new_list=[]
+    i=0
+    j=0
+    
+    while (i<len(left)) & (j<len(right)):
+        if left[i]>right[j]:
+            new_list.append(right[j])
+            j+=1
+        else:
+            new_list.append(left[i])
+            i+=1
+    while (j<len(right)):
+            new_list.append(right[j])
+            j+=1
+    while (i<len(left)):
+            new_list.append(left[i])
+            i+=1
+    return new_list
+    
+for i in sort(li):
+    print(i)
